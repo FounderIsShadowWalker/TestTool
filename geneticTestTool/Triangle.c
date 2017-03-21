fc92b740bc16e426998f3b2dd40b9d237b28ec6e
ShadowWalker <1528021521@qq.com>
2017-03-21 20:40:18 +0800

    revise
    revise

---
+++ geneticTestTool/Triangle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void triangle(int a, int b, int c){
+    if(a+b<c||a+c<b||b+c<a)
+        printf("not triangle");
+    else if(a==c && a==b)
+        printf("equilateral triangle");
+    else if((a==b&&(a!=c))||(a==c&&(b!=c))||(b==c&&(a!=c)))
+        printf("isosceles triangle");
+    else if(a*a+b*b==c*c || a*a+c*c==b*b || c*c+b*b==a*a){
+        printf("right-angled triangle");
+    }
+    else{
+        printf("normal triangle");
+    }
+}
+int main(int argc, const char * argv[]) {
+    triangle(-13,4,-3);
+    return 0;
+}
---
+++ geneticTestTool/src/regex.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class regex {
+	  public static void main(String args[]){
+		  float result = (float) 1/3;
+		  System.out.println(Math.round(result));
+		  
+	  }
+}
